5ab75ec2dfb45824f2fadddab61a9f15d9b964ab
Jeff Greco <jeffpgreco@gmail.com>
2019-08-02 15:39:28 -0400

    takes user input

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+var computerGuess = ["abcdefghijklmnopqrstuvwvyz"];
+
+var wins = 0;
+var losses = 0;
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+
+    console.log(userGuess)
+
+
+}
